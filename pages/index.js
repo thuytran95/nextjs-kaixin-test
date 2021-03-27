9008f987572ce130490dbb72801a524b9181c84b
Tran Thuy <thuytt0295@gmail.com>
2021-03-27 08:33:37 +0700

    add source khoa hoc

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,33 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://unpkg.com/video.js/dist/video-js.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <script src="js/videojs-core.js" type="text/javascript"></script>
+        <script
+          src="js/videojs-http-streaming.js"
+          type="text/javascript"
+        ></script>
+        <script src="js/control-video.js" type="text/javascript"></script>
       </Head>
       <Header />
       <h1>Trang home</h1>
+      <video-js
+        id="my_video_1"
+        class="vjs-default-skin vjs-16-9"
+        controls
+        preload="auto"
+        width="100%"
+        height="350"
+      >
+        <source
+          src="https://file.mentor.vn/files/lessons/output/file-1614840748161/playlist.m3u8"
+          type="application/x-mpegURL"
+        />
+      </video-js>
       <MessengerChat pageId="105674048286261" htmlRef="fb-msgr" />
     </div>
   );
---
+++ public/js/control-video.js
@@ -0,0 +1,7 @@
+var player = videojs("my_video_1", {
+  html5: {
+    vhs: {
+      overrideNative: true,
+    },
+  },
+});
